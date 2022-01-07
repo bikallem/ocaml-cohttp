a99ff8e2d2a76bdcac343b17b8f9199cee4146cc
Bikal Lem <gbikal+git@gmail.com>
2022-01-07 16:48:01 +0000

    cohttp-eio: remove redundant functions

--- cohttp-eio/src/io.mli
+++ cohttp-eio/src/io.mli
@@ -2,7 +2,6 @@ type +'a t
 type ic = In_channel.t
 type oc = Eio.Flow.write
 type conn = Eio.Flow.two_way
-type error = exn
 
 val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
 val return : 'a -> 'a t
@@ -10,5 +9,3 @@ val read_line : ic -> string option t
 val read : ic -> int -> string t
 val write : oc -> string -> unit t
 val flush : oc -> unit t
-val catch : (unit -> 'a t) -> ('a, error) result t
-val pp_error : Format.formatter -> error -> unit

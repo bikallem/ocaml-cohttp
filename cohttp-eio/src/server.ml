982a2d1d11989dc81ee13a54e6d2ada42b8bfb7e
Bikal Lem <gbikal+git@gmail.com>
2022-01-10 17:34:29 +0000

    add create to create server. run runs the server

--- cohttp-eio/src/server.ml
+++ cohttp-eio/src/server.ml
@@ -1,9 +1,18 @@
 open Eio.Std
 
-type t = < Eio.Flow.two_way ; Eio.Flow.close >
+type t = {
+  domains : int;
+  port : int;
+  backlog : int;
+  error_handler : Eio.Net.Sockaddr.t -> exn -> unit;
+  request_handler :
+    Eio.Std.Switch.t -> Eio.Net.Sockaddr.t -> Cohttp.Request.t -> unit;
+  closed : bool Atomic.t;
+}
+
 type response = Cohttp.Response.t * Cohttp.Body.t [@@deriving sexp]
 
-let close t = Eio.Flow.close t
+let close t = ignore @@ Atomic.compare_and_set t.closed false true
 
 let cpu_count =
   match Sys.os_type with
@@ -37,28 +46,44 @@ let connection_handler sw (flow : < Eio.Flow.two_way ; Eio.Flow.close >)
     | `Ok req -> request_handler sw client_addr req
   with exn -> on_error client_addr exn
 
-let run_accept_loop ~sw ~backlog ~port ~on_error ~request_handler env =
+let run_accept_loop (t : t) sw env =
   let domain_mgr = Eio.Stdenv.domain_mgr env in
   Fibre.fork ~sw @@ fun () ->
   Eio.Domain_manager.run domain_mgr @@ fun () ->
   let net = Eio.Stdenv.net env in
-  let sockaddr = `Tcp (Unix.inet_addr_loopback, port) in
+  let sockaddr = `Tcp (Unix.inet_addr_loopback, t.port) in
   let ssock =
-    Eio.Net.listen ~reuse_addr:true ~reuse_port:true ~backlog ~sw net sockaddr
+    Eio.Net.listen ~reuse_addr:true ~reuse_port:true ~backlog:t.backlog ~sw net
+      sockaddr
   in
-  while true do
-    Eio.Net.accept_sub ~sw ssock
-      ~on_error:(fun exn -> Printf.printf "%s" (Printexc.to_string exn))
+  let on_accept_error exn =
+    Printf.fprintf stderr "Error while accepting connection: %s"
+      (Printexc.to_string exn)
+  in
+  while not (Atomic.get t.closed) do
+    Eio.Net.accept_sub ~sw ssock ~on_error:on_accept_error
       (fun ~sw socket client_addr ->
-        connection_handler sw socket client_addr on_error request_handler)
+        connection_handler sw socket client_addr t.error_handler
+          t.request_handler)
   done
 
+let create ?(backlog = 10_000) ?(domains = cpu_count) ~port ~error_handler
+    request_handler : t =
+  {
+    domains;
+    port;
+    backlog;
+    error_handler;
+    request_handler;
+    closed = Atomic.make false;
+  }
+
 (* wrk2 -t10 -c400 -d30s -R2000 http://localhost:3000 *)
-let run ?(backlog = 10_000) ~port ~on_error request_handler =
+let run (t : t) =
   Eio_main.run @@ fun env ->
   Switch.run @@ fun sw ->
-  (* Run accept loop on domain0 without creating a Domaint.t *)
-  run_accept_loop ~sw ~backlog ~port ~on_error ~request_handler env;
-  for _ = 2 to cpu_count do
-    run_accept_loop ~sw ~backlog ~port ~on_error ~request_handler env
+  (* Run accept loop on domain0 without creating a Domain.t *)
+  run_accept_loop t sw env;
+  for _ = 2 to t.domains do
+    run_accept_loop t sw env
   done
--- cohttp-eio/src/server.mli
+++ cohttp-eio/src/server.mli
@@ -3,9 +3,12 @@ type response = Cohttp.Response.t * Cohttp.Body.t [@@deriving sexp_of]
 
 val close : t -> unit
 
-val run :
+val create :
   ?backlog:int ->
+  ?domains:int ->
   port:int ->
-  on_error:(Eio.Net.Sockaddr.t -> exn -> unit) ->
+  error_handler:(Eio.Net.Sockaddr.t -> exn -> unit) ->
   (Eio.Std.Switch.t -> Eio.Net.Sockaddr.t -> Cohttp.Request.t -> unit) ->
-  unit
+  t
+
+val run : t -> unit

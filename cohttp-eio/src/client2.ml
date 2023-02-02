f501b3e58cff95c368a9c5623df71907de31e16a
Bikal Lem <gbikal@gmail.com>
2023-02-02 16:52:41 +0000

    cohttp-eio(client) : define Client.t and Client.v

--- cohttp-eio/src/client2.ml
+++ cohttp-eio/src/client2.ml
@@ -3,6 +3,35 @@ module Buf_write = Eio.Buf_write
 
 type response = Http.Response.t * Buf_read.t
 
+(* TODO bikal implement redirect functionality
+   TODO bikal implement cookie jar functionality
+   TODO bikal allow user to override redirection
+   TODO bikal implement connection caching functionality *)
+
+class virtual t =
+  object
+    (* TODO enable this method when redirect is implemented
+
+       method virtual continue_redirect
+               : 'a 'b.
+                 current:((#Body2.writer as 'a) #Request.client_request as 'b) ->
+                 previous:'b list ->
+                 [ `Continue | `Stop of string ] *)
+    method virtual timeout : Eio.Time.Timeout.t
+    method virtual buf_read_initial_size : int
+    method virtual buf_write_initial_size : int
+    method virtual pipeline_requests : bool
+  end
+
+let v ?(timeout = Eio.Time.Timeout.none) ?(buf_read_initial_size = 0x1000)
+    ?(buf_write_initial_size = 0x1000) ?(pipeline_requests = true) () =
+  object
+    method timeout = timeout
+    method buf_read_initial_size = buf_read_initial_size
+    method buf_write_initial_size = buf_write_initial_size
+    method pipeline_requests = pipeline_requests
+  end
+
 (* response parser *)
 
 let is_digit = function '0' .. '9' -> true | _ -> false
@@ -27,26 +56,25 @@ let response buf_read =
   let headers = Rwer.http_headers buf_read in
   Http.Response.make ~version ~status ~headers ()
 
-let do_request_response ?pipeline_requests conn req body =
+let do_request_response (t : #t) conn req body =
   let initial_size = 0x1000 in
   Buf_write.with_flow ~initial_size conn (fun writer ->
-      Request.write ?pipeline_requests req body writer;
+      Request.write ~pipeline_requests:t#pipeline_requests req body writer;
       let reader = Eio.Buf_read.of_flow ~initial_size ~max_size:max_int conn in
       let response = response reader in
       (response, reader))
 
 let call :
-    ?pipeline_requests:bool ->
+    #t ->
     conn:#Eio.Flow.two_way ->
     (#Body2.writer as 'a) Request.client_request ->
     'a ->
     response =
- fun ?pipeline_requests ~conn req body ->
-  do_request_response ?pipeline_requests conn req body
+ fun t ~conn req body -> do_request_response t conn req body
 
-let with_call net (r : (#Body2.writer as 'a) Request.client_request) (body : 'a)
-    f =
+let with_call (t : #t) net (r : (#Body2.writer as 'a) Request.client_request)
+    (body : 'a) f =
   let host, port = Request.client_host_port r in
   let service = match port with Some x -> string_of_int x | None -> "80" in
   Eio.Net.with_tcp_connect ~host ~service net (fun conn ->
-      f @@ do_request_response ~pipeline_requests:false conn r body)
+      f @@ do_request_response t conn r body)
--- cohttp-eio/src/client2.mli
+++ cohttp-eio/src/client2.mli
@@ -1,11 +1,35 @@
+(** [Client] is a HTTP client *)
+
+class virtual t :
+  object
+    method virtual timeout : Eio.Time.Timeout.t
+    (** [timeout] specifies total time limit for establishing a connection,
+        calling a request and getting a response back.
+
+        A client request is cancelled if the specified timeout limit is
+        exceeded. *)
+
+    method virtual buf_read_initial_size : int
+    (** [buf_read_initial_size] is the buffered reader initial size. *)
+
+    method virtual buf_write_initial_size : int
+    (** [buf_write_initial_size] is the buffered writer iniital size. *)
+
+    method virtual pipeline_requests : bool
+  end
+
+val v :
+  ?timeout:Eio.Time.Timeout.t ->
+  ?buf_read_initial_size:int ->
+  ?buf_write_initial_size:int ->
+  ?pipeline_requests:bool ->
+  unit ->
+  t
+
 type response = Http.Response.t * Eio.Buf_read.t
 
 val call :
-  ?pipeline_requests:bool ->
-  conn:#Eio.Flow.two_way ->
-  'a Request.client_request ->
-  'a ->
-  response
+  #t -> conn:#Eio.Flow.two_way -> 'a Request.client_request -> 'a -> response
 
 val with_call :
-  Eio.Net.t -> 'a Request.client_request -> 'a -> (response -> 'b) -> 'b
+  #t -> Eio.Net.t -> 'a Request.client_request -> 'a -> (response -> 'b) -> 'b

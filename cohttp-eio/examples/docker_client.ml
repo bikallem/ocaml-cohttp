00e118583afd49cbb707160d30a05a432fc49543
Bikal Lem <gbikal@gmail.com>
2023-02-03 16:18:03 +0000

    cohttp-eio(examples): update docker_client.ml to use Client2

--- cohttp-eio/examples/docker_client.ml
+++ cohttp-eio/examples/docker_client.ml
@@ -1,20 +1,19 @@
-module Switch = Eio.Switch
-module Net = Eio.Net
-module Stdenv = Eio.Stdenv
-module Client = Cohttp_eio.Client
-module Response = Http.Response
-module Status = Http.Status
+open Cohttp_eio
 
 let () =
   Eio_main.run @@ fun env ->
-  Switch.run @@ fun sw ->
+  Eio.Switch.run @@ fun sw ->
   let addr = `Unix "/var/run/docker.sock" in
-  let conn = Net.connect ~sw env#net addr in
-  let res = Client.get ~conn ~host:"docker" env "/version" in
-  let code = fst res |> Response.status |> Status.to_int in
+  let conn = Eio.Net.connect ~sw env#net addr in
+  let req =
+    Request.client_request ~host:"docker" ~resource:"/version" Method.Get
+      Body2.none
+  in
+  let res = Client2.call ~conn req in
+  let code = fst res |> Http.Response.status |> Http.Status.to_int in
   Printf.printf "Response code: %d\n" code;
   Printf.printf "Headers: %s\n"
-    (fst res |> Response.headers |> Http.Header.to_string);
+    (fst res |> Http.Response.headers |> Http.Header.to_string);
   let body = Client.read_fixed res in
   Printf.printf "Body of length: %d\n" (String.length body);
   print_endline ("Received body\n" ^ body)
